f2572c877a85f2d8aac14fe135f81b66422ec1b4
Kwabena Agyeman <kagyeman@ymail.com>
2021-05-17 17:25:35 +0000

    updated firebase rules and made changes to assests in the firestore database

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,13 +1,23 @@
 import firebase from 'firebase';
 
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyA9BnlX96fMf7XiUVCFRsoQzG8DGERJkeY',
+//   authDomain: 'disneyplus-clone-a33d5.firebaseapp.com',
+//   projectId: 'disneyplus-clone-a33d5',
+//   storageBucket: 'disneyplus-clone-a33d5.appspot.com',
+//   messagingSenderId: '37918794208',
+//   appId: '1:37918794208:web:dbe9842dfe1dda522a4b85',
+//   measurementId: 'G-DRVLJKWRWG',
+// };
+
 const firebaseConfig = {
-  apiKey: 'AIzaSyA9BnlX96fMf7XiUVCFRsoQzG8DGERJkeY',
-  authDomain: 'disneyplus-clone-a33d5.firebaseapp.com',
-  projectId: 'disneyplus-clone-a33d5',
-  storageBucket: 'disneyplus-clone-a33d5.appspot.com',
-  messagingSenderId: '37918794208',
-  appId: '1:37918794208:web:dbe9842dfe1dda522a4b85',
-  measurementId: 'G-DRVLJKWRWG',
+  apiKey: 'AIzaSyC4IG2OzkzXvSxM6ccZY5eKUUP7YXCI8w8',
+  authDomain: 'disney-plus-clone-4cb84.firebaseapp.com',
+  projectId: 'disney-plus-clone-4cb84',
+  storageBucket: 'disney-plus-clone-4cb84.appspot.com',
+  messagingSenderId: '713285823658',
+  appId: '1:713285823658:web:d66d8650d1764b921e34d3',
+  measurementId: 'G-R2TJ9HKEWX',
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
--- src/components/Viewers/Viewers.component.jsx
+++ src/components/Viewers/Viewers.component.jsx
@@ -7,23 +7,44 @@ import view3 from '../../Assets/images/viewers-national.png';
 import view4 from '../../Assets/images/viewers-pixar.png';
 import view5 from '../../Assets/images/viewers-starwars.png';
 
+import disney from '../../Assets/videos/1564674844-disney.mp4';
+import marvel from '../../Assets/videos/1564676115-marvel.mp4';
+import natgeo from '../../Assets/videos/1564676296-national-geographic.mp4';
+import pixar from '../../Assets/videos/1564676714-pixar.mp4';
+import starwars from '../../Assets/videos/1608229455-star-wars.mp4';
+
 const Viewers = () => {
   return (
     <div className='viewers-container'>
-      <div className='viewers-wrapper disney'>
+      <div className='viewers-wrapper'>
         <img src={view1} alt='' />
+        <video autoPlay muted loop playsInline>
+          <source src={disney} type='video/mp4' />
+        </video>
       </div>
       <div className='viewers-wrapper'>
         <img src={view2} alt='' />
+        <video autoPlay muted loop playsInline>
+          <source src={marvel} type='video/mp4' />
+        </video>
       </div>
       <div className='viewers-wrapper'>
         <img src={view3} alt='' />
+        <video autoPlay muted loop playsInline>
+          <source src={natgeo} type='video/mp4' />
+        </video>
       </div>
       <div className='viewers-wrapper'>
         <img src={view4} alt='' />
+        <video autoPlay muted loop playsInline>
+          <source src={pixar} type='video/mp4' />
+        </video>
       </div>
       <div className='viewers-wrapper'>
         <img src={view5} alt='' />
+        <video autoPlay muted loop playsInline>
+          <source src={starwars} type='video/mp4' />
+        </video>
       </div>
     </div>
   );
